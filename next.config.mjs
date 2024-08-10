c25a13f98acba4e742d47878a3e564028afea160
Audun Beck <17355570+AudunBeck@users.noreply.github.com>
2024-08-10 13:22:28 +0200

    feat: add plausible analytics with next-plausible

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
+import { withPlausibleProxy } from "next-plausible";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withPlausibleProxy({
+  customDomain: process.env.PLAUSIBLE_ANALYTICS_DOMAIN,
+})(nextConfig);
