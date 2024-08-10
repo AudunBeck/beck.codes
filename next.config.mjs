40f7f5d488eb36ada76d06f7051e969829337b2c
Audun Beck <17355570+AudunBeck@users.noreply.github.com>
2024-08-10 14:28:34 +0200

    test: use normal scripttag

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
-import { withPlausibleProxy } from "next-plausible";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withPlausibleProxy({
-  subdirectory: process.env.PLAUSIBLE_ANALYTICS_SUBDIRECTORY,
-  customDomain: process.env.PLAUSIBLE_ANALYTICS_DOMAIN,
-})(nextConfig);
+export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import PlausibleProvider from "next-plausible";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <PlausibleProvider
-          selfHosted
-          domain={process.env.PLAUSIBLE_SITE_DOMAIN || ""}
+        <Script
+          strategy="beforeInteractive"
+          defer
+          data-domain={process.env.PLAUSIBLE_SITE_DOMAIN}
+          data-api={`${process.env.PLAUSIBLE_ANALYTICS_DOMAIN}/${process.env.PLAUSIBLE_ANALYTICS_SUBDIRECTORY}/event`}
+          src={`${process.env.PLAUSIBLE_ANALYTICS_DOMAIN}/${process.env.PLAUSIBLE_ANALYTICS_SUBDIRECTORY}/script.js`}
         />
       </head>
       <body className={inter.className}>{children}</body>
