c8fd27c0ff56ca611f2dcfa381aadbb94f58aa51
Audun Beck <17355570+AudunBeck@users.noreply.github.com>
2024-08-24 20:17:51 +0200

    feat: add today as output from RelativeDate

--- src/components/RelativeDate.tsx
+++ src/components/RelativeDate.tsx
@@ -6,6 +6,13 @@ import { formatDistanceToNow, isMatch, parse } from "date-fns";
 export function RelativeDate({ rawDate }: { rawDate: MdxDateType }) {
   if (isMatch(rawDate, "yyyy-MM-dd")) {
     const date = parse(rawDate, "yyyy-MM-dd", new Date());
+    const oneDay = 1000 * 3600 * 24;
+    const distance = Date.now() - date.getTime();
+
+    if (distance < oneDay && distance > 0) {
+      return "today";
+    }
+
     const relativeDate = formatDistanceToNow(date, {
       addSuffix: true,
     });
