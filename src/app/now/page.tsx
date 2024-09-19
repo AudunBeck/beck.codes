767ec72bc45a5e7083540da533831698e678fb0d
Audun Sagstuen Beck <17355570+AudunBeck@users.noreply.github.com>
2024-09-19 21:55:40 +0200

    feat: started on now page

--- src/app/now/page.tsx
+++ src/app/now/page.tsx
@@ -1,10 +1,26 @@
+import fs from "node:fs";
+import path from "node:path";
+import type { FrontmatterType } from "@/utils/mdxUtils";
+import { compileMDX } from "next-mdx-remote/rsc";
+
 export default async function AboutPage() {
+  const nowPath = path.join(process.cwd(), "src/app/now/now.mdx");
+  const source = fs.readFileSync(nowPath);
+  const { content } = await compileMDX<FrontmatterType>({
+    source: source,
+    options: { parseFrontmatter: true },
+  });
   return (
-    <main className="px-4 md:dpx-16">
-      <h1 className="text-4xl md:text-6xl mb-6 font-semibold mt-8">Now</h1>
-      <h2 className="text-2xl md:text-3xl">
-        A log of what I have been making, reading and thinking about, soon...
-      </h2>
-    </main>
+    <div className="px-4 md:px-16">
+      <header>
+        <h1 className="text-4xl md:text-6xl mb-6 font-semibold mt-8">Now</h1>
+        <h2 className="text-2xl md:text-3xl">
+          A log of what I have been making, reading and thinking about.
+        </h2>
+      </header>
+      <main className="mt-8 prose md:prose-lg prose-slate dark:prose-invert">
+        {content}
+      </main>
+    </div>
   );
 }
