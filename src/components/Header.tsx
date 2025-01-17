b54f03238622b3f4c1de506dafe5e30296b4dbb4
Audun Beck <17355570+AudunBeck@users.noreply.github.com>
2025-01-17 16:06:52 +0100

    style: add gradient to layout title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 export async function Header() {
   return (
     <nav className="flex justify-between px-4 py-8 md:px-16">
-      <Link href={"/"} className="text-2xl">
-        beck.codes
+      <Link href={"/"}>
+        <span className="bg-linear-to-r from-accent to-secondary bg-clip-text font-semibold text-2xl text-transparent">
+          beck.codes
+        </span>
       </Link>
       <div className="flex gap-4 text-lg">
         <Link href={"/now"}>Now</Link>
