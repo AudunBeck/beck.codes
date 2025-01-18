de58c3e21c253fba9565aafa739eefa8f50c69b6
Audun Beck <17355570+AudunBeck@users.noreply.github.com>
2025-01-18 16:27:09 +0100

    note: add to about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,7 +14,25 @@ export default async function AboutPage() {
         </em>
       </h2>
       <div className="prose prose-slate md:prose-lg dark:prose-invert mt-8">
-        <p className="">I am not sure what to put here yet.</p>
+        <p>
+          I work as a webdev, focusing on the full experience of the technical
+          stack.
+          <br />
+          Starting at the development team's workflow and choice of tech.
+        </p>
+        <p>
+          I'm a senior consultant at{" "}
+          <a href="https://www.netlight.com/" className="text-nl-purple">
+            Netlight
+          </a>
+          , a consultancy that focuses on learning and collective intelligence
+          in the technology industry.
+        </p>
+        <p>
+          When I don't work, I enjoy spending my time tinkering with software,
+          play board games and tabletop rpgs.
+          <br />I do also read a lot, and sometimes write.
+        </p>
       </div>
     </main>
   );
